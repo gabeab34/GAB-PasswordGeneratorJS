a3368ae49a0f3c34298440367f80d8d1a58254d3
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-05-12 21:08:04 -0400

    first prompt working

--- script.js
+++ script.js
@@ -3,15 +3,23 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var userinput;
+
+
 
 // Write password to the #password input
 function writePassword() {
+  userinput = parseInt(prompt("Choose between 8 and 128 characters for your password"));
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var pslength = 8
+  let ps = window.prompt ("Password Length?" );
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
