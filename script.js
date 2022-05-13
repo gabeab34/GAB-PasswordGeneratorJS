2e4dd19777fe221bba5b4ecfb8be442ebb9a438a
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-05-12 22:26:37 -0400

    added additional prompts

--- script.js
+++ script.js
@@ -10,7 +10,15 @@ var userinput;
 // Write password to the #password input
 function writePassword() {
   userinput = parseInt(prompt("Choose between 8 and 128 characters for your password"));
-  
+  if (!userinput) {
+    alert("Value needed");
+  } else if (userinput < 8 || userinput > 128) {
+    userinput = parseInt(prompt("Choose between 8 and 128 characters for your password"));
+  }
+  userinput = parseInt(prompt("Include lowercase?"));
+  userinput = parseInt(prompt("Include uppercase?"));
+  userinput = parseInt(prompt("Include numeric characters?"));
+  userinput = parseInt(prompt("Include special characters?"));
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
