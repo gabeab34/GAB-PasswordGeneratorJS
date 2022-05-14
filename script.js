eaffcb69435cf320afc1085e7bbd949a07020d4d
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-05-13 22:08:36 -0400

    revamped structure, alert displays password

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
-// Assignment code here
+/// Assignment code here
 
+const selections = {
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  number: '0123456789',
+  symbol: "!$%&'()*+,-./:;<=>?@[]^_`{|}~",
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var userinput;
-
-
 
 // Write password to the #password input
 function writePassword() {
-  userinput = parseInt(prompt("Choose between 8 and 128 characters for your password"));
-  if (!userinput) {
-    alert("Value needed");
-  } else if (userinput < 8 || userinput > 128) {
-    userinput = parseInt(prompt("Choose between 8 and 128 characters for your password"));
-  }
-  userinput = parseInt(confirm("Include lowercase?"));
-  userinput = parseInt(confirm("Include uppercase?"));
-  userinput = parseInt(confirm("Include numeric characters?"));
-  userinput = parseInt(confirm("Include special characters?"));
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var pslength = 8
-  
+  var passwordselection = "";
 
-  passwordText.value = password;
+  var pwlength = window.prompt("Please input a number between 8 and 128")
+
+  var lcase = window.confirm("Include lowercase?")
+  if (lcase) {
+    passwordselection += selections.lowercase;
+  };
+
+  var ucase = window.confirm("Include uppercase?")
+  if (ucase) {
+    passwordselection += selections.uppercase;
+  };
+
+  var symbols = window.confirm("Include symbols?")
+  if (symbols) {
+    passwordselection += selections.symbol;
+
+  };
+
+  var numbers = window.confirm("Include numbers?")
+  if (numbers) {
+    passwordselection += selections.number;
+
+  };
+
+  var password = "";
+  for (let i = 0; i < pwlength; i++) {
+    password += passwordselection[Math.floor(Math.random() * passwordselection.length)]
+  
+  }
+  alert(password)
+  
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
+generateBtn.addEventListener("click", writePassword);
