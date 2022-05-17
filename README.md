8ef19b324bb9cff155ca0e886b36be0a9fda4d1b
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-05-16 20:12:51 -0400

    added comments to js code and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# GAB-challenge-3
+# GAB-PasswordGenerator-JS-C3
+
+In this challenge I created a password generator using javascript. Once I got it to successfully generate a password and write it to the page, I came up with a series of alerts and prompts to make sure that the user would get the expected output from the function. 
+
+![challenge3](JS-PWG-SS.png "My screenshot" )
+<a href=https://gabeab34.github.io/GAB-PasswordGenerator-JS-C3/ alt="my webpage"> My Webpage
--- script.js
+++ script.js
@@ -9,55 +9,58 @@ const selections = {
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
   
   var passwordselection = "";
-
+// First prompt box
   var pwlength = prompt("Please input a number between 8 and 128")
   if (!pwlength) {
     return writePassword + alert("You must enter a number")
   };
+// Followup prompt and alert if instructions not followed
   if (pwlength < 8 || pwlength > 128) {
     pwlength = prompt("Choose a number between 8 and 128")
   } if (pwlength < 8 || pwlength > 128) {
     return writePassword + alert("Please follow instructions") 
   };
-  
+  // First Confirm box
   var lcase = confirm("Include lowercase? Ok for yes, cancel for no")
   if (lcase) {
     passwordselection += selections.lowercase;
   };
-
+  // Second Confirm box
   var ucase = confirm("Include uppercase? Ok for yes, cancel for no")
   if (ucase) {
     passwordselection += selections.uppercase;
   };
-
+  // Third Confirm box
   var numbers = confirm("Include numbers? Ok for yes, cancel for no")
   if (numbers) {
     passwordselection += selections.number;
   };
-
+  // Fourth Confirm box
   var symbols = confirm("Include special characters? Ok for yes, cancel for no")
   if (symbols) {
     passwordselection += selections.symbol;
   };
-
+  // if statement that returns to top of function if user selects no character types
   if (!lcase && !ucase && !numbers && !symbols) {
   return writePassword + alert("You must choose at least one character type")
   };
-
+  // Formula for rng
   var password = "";
   for (let i = 0; i < pwlength; i++) {
     password += passwordselection[Math.floor(Math.random() * passwordselection.length)]
   
   }
+  // writes password to alert box (not used in final build)
   // alert(password)
- 
+
+
+  // writes password to page
   document.querySelector("#password").textContent = password;
 
 }
